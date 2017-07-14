9a073f94be7ba3280a219fc50cdeb192df4f9ea8
jianyu-m <jianyu@hku.hk>
2017-07-14 14:30:00 +0800

    contruct B correctly

--- exact.py
+++ exact.py
@@ -4,6 +4,9 @@ from numpy.linalg import matrix_rank
 
 
 # return max geo-multiplicity and its value
+import ERGenerator
+
+
 def max_e(arr, unique_arr, n):
     max_multi = -1
     max_lambda = 0
@@ -55,6 +58,35 @@ def construct_v(arr, n):
     return z
 
 
+def compare_independent(arr, row):
+    independent = True
+    for arr_row in arr:
+        if max(arr_row - row) == 0:
+            independent = False
+            break
+    return independent
+
+
+# input is in the ... form
+def construct_v_opt(arr, n):
+    l = []
+    arr_l = []
+    if max(arr[0]) == 0:
+        l.append(0)
+    else:
+        arr_l.append(arr[0])
+    for i in range(1, len(arr)):
+        if compare_independent(arr_l, arr[i]):
+            arr_l.append(arr[i])
+        else:
+            l.append(i)
+
+    z = numpy.zeros((n, len(l)))
+    for i, li in enumerate(l):
+        z[li][i] = 1
+    return z
+
+
 def process_graph(graph):
     n_r, n_c = graph.shape
     if not n_r == n_c:
@@ -64,16 +96,19 @@ def process_graph(graph):
     lamda, v = numpy.linalg.eig(graph)
     unique_lamda = numpy.unique(lamda)
     lamda_gm, lamda_gm_c = max_e(graph, unique_lamda, n)
-    B = construct_v(element_column(graph - lamda_gm * numpy.identity(n), n), n)
-
+    blist = []
+    for lam in unique_lamda:
+        B = construct_v(element_column(graph - lam * numpy.identity(n), n), n)
+        print(lam)
+        blist.append(B)
+    ba = numpy.concatenate(blist, axis=1)
     # verify this result
-    _, b_c = B.shape
+    _, b_c = ba.shape
     r = matrix_rank(numpy.concatenate(
-        (lamda_gm * numpy.identity(n), numpy.zeros((n, b_c))), axis=1) -
-                    numpy.concatenate((A, B), axis=1))
+        (lamda_gm * numpy.identity(n) - A, ba), axis=1))
     print(lamda_gm, lamda_gm_c, r)
     assert r == n
-    return B
+    return ba
 
 
 if __name__ == "__main__":
@@ -85,7 +120,7 @@ if __name__ == "__main__":
                      (1, 0, 0, 0, 1, 0)], numpy.float32)
 
     A = numpy.array([(0, 0, 0, 0, 0, 0),
-                     (1, 0, 0, 0, 0, 0),
+                     (0, 1, 0, 0, 0, 0),
                      (1, 0, 0, 0, 0, 0),
                      (1, 0, 0, 0, 0, 0),
                      (1, 0, 0, 0, 0, 1),
