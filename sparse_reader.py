ab2422db282dfc45847c4c65cc9a8edf283eee52
jianyu-m <jianyu@hku.hk>
2017-07-18 00:29:32 +0800

    ignore comment

--- sparse_reader.py
+++ sparse_reader.py
@@ -8,6 +8,8 @@ def file_to_matrix(filename, n):
         line_m = f.read()
         lines = line_m.split("\n")
         for line in lines:
+            if line.startswith("#"):
+                continue
             line_arr = line.strip().split(" ")
             x, y, z = int(line_arr[0]), int(line_arr[1]), int(line_arr[2])
             matrix[x - 1][y - 1] = z
