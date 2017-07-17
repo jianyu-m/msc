a834c4a880a1a6fd9d27dcff339a01d2d9c48017
jianyu-m <jianyu@hku.hk>
2017-07-18 00:28:34 +0800

    eps

--- exact.py
+++ exact.py
@@ -4,6 +4,7 @@ from numpy.linalg import matrix_rank
 
 import sparse_reader
 
+global eps
 
 # calculate geo-multiplicity
 def lambda_geo(arr, unique_arr, n):
@@ -15,7 +16,7 @@ def lambda_geo(arr, unique_arr, n):
 
 def get_none_zero(arr, to):
     for i, val in enumerate(arr):
-        if val != 0 and i >= to:
+        if val > eps and i >= to:
             return i, val
     return -1, 0
 
@@ -35,38 +36,6 @@ def element_column(arr, n):
     return arr
 
 
-def construct_v(arr, n):
-    base = matrix_rank(arr[0])
-    l = []
-    if base == 0:
-        l.append(0)
-    for i in range(1, len(arr)):
-        cal_rank = matrix_rank(arr[0:i + 1, :], eps)
-        if cal_rank > base:
-            base = cal_rank
-        else:
-            l.append(i)
-
-    z = numpy.zeros((n, len(l)))
-    for i, li in enumerate(l):
-        z[li][i] = 1
-    return z
-
-
-def v_count(arr):
-    base = matrix_rank(arr[0])
-    num = 0
-    if base == 0:
-        num += 1
-    for i in range(1, len(arr)):
-        cal_rank = matrix_rank(arr[0:i + 1, :])
-        if cal_rank > base:
-            base = cal_rank
-        else:
-            num += 1
-    return num
-
-
 def set_v(arr, v):
     base = matrix_rank(arr[0])
     if base == 0:
@@ -79,21 +48,14 @@ def set_v(arr, v):
             v[i] = True
 
 
-def compare_independent(arr, row):
-    independent = True
-    for arr_row in arr:
-        if max(arr_row - row) == eps:
-            independent = False
-            break
-    return independent
-
-
 def process_graph(graph):
+    global eps
     n_r, n_c = graph.shape
     if not n_r == n_c:
         print("wrong matrix")
         return
     n = n_r
+    eps = numpy.finfo(graph.dtype).eps * n
     lamda, v = numpy.linalg.eig(graph)
     unique_lamda = numpy.unique(lamda)
     lambda_arr = lambda_geo(graph, unique_lamda, n)
@@ -113,7 +75,8 @@ def process_graph(graph):
     for vi in v:
         if vi:
             msc += 1
-    return controller, msc, msc_upper
+    # controller number | msc | upper_bound | unique_lambda | upper - lower
+    return controller, msc, min(msc_upper, n), len(unique_lamda) - 1, min(msc_upper, n) - controller
 
 if __name__ == "__main__":
     A = numpy.array([(0, 1, 1, 1, 1, 1),
@@ -131,9 +94,15 @@ if __name__ == "__main__":
                      (1, 0, 0, 0, 0, 0)], numpy.float64)
 
     # input is a transmission matrix representing the graph
-    # msc = process_graph(sparse_reader.file_to_matrix("dataset/Organizational-Consulting.txt", 46))
-    msc = process_graph(sparse_reader.file_to_matrix("dataset/Trust-prison inmate.txt", 67))
+    # msc = process_graph(sparse_reader.file_to_matrix("celegans_metabolic[503].net", 503))
+    # msc = process_graph(sparse_reader.file_to_matrix("dataset/Electronic circuits-s208a[122].txt", 122))
+    # msc = process_graph(sparse_reader.file_to_matrix("dataset/Electronic circuits-s420a[252].txt", 252))
+    # msc = process_graph(sparse_reader.file_to_matrix("dataset/Electronic circuits-s838a[515].txt", 515))
+    # msc = process_graph(sparse_reader.file_to_matrix("dataset/Organizational-Consulting[46].txt", 46))
+    # msc = process_graph(sparse_reader.file_to_matrix("dataset/Organizational-Freeman[46].txt", 46))
+    # msc = process_graph(sparse_reader.file_to_matrix("dataset/Trust-prison inmate[67].txt", 67))
     # msc = process_graph(A)
+    print("controller number | msc | upper_bound | unique_lambda - 1 <= upper - lower")
     print(msc)
     # for l in un_lam:
     #     print(sympy.Matrix(l * numpy.identity(N) - A))
